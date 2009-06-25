41bb644464aee321bb9db5b049a983c04f01457b
shanehill00 <shanehill00@10444eee-95d0-11de-99ed-9d3e36ffeeb7>
2009-06-25 18:16:28 +0000

    documentation cleanup

--- trunk/utils/dfs.php
+++ trunk/utils/dfs.php
@@ -11,25 +11,24 @@ $params = PHPDFS_Helper::getParamsFromUrl();
 $dfs = new PHPDFS( $config, $params );
 
 // get the data from stdin and put it in a temp file
-// we will disconnect the client at this point and continue
-// with replication and data forwarding if we are configged to do so
+// we will disconnect the client at this point if we are configured to do so
 // otherwise we hang on to the client, which in most cases is really bad
-// because you stay connected until the replication chain is completed
+// because you might stay connected until the replication chain is completed
 $dfs->spoolData( );
 
-// save the data to the appropriate directory and emove the spooled file
+// save the data to the appropriate directory and remove the spooled file
 // but only if we are a targetNode, otherwise DO NOTHING
 $dfs->saveData( );
 
 // forward the data on to the next node
 // the reasons for forwarding are:
 //
-// we are NOT a target node and are just the first node
+// we are NOT a targetNode and are just the first node
 // to receive the upload, so we forward the data to the first targetNode
 // and remove the spooled file
 //
-// or we are a targetNode and need to fulfill the replication requirements
-// and forward data to the next targetNode in our list.
-// if we are the last replication target, we DO NOTHING.
+// OR we are a targetNode and need to fulfill the replication requirements
+// so, we forward data to the next targetNode in our list.
+// however, if we are the last replication targetNode, we DO NOTHING.
 $dfs->forwardData( );
 
