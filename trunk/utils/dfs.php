876f3ee368f592ffc1e4e02f33f3dcd31e867117
shanehill00 <shanehill00@10444eee-95d0-11de-99ed-9d3e36ffeeb7>
2009-06-25 17:33:25 +0000

    code cleanup and expanded config values to allow for controlling the spooling behavior

--- trunk/utils/dfs.php
+++ trunk/utils/dfs.php
@@ -1,80 +1,35 @@
 <?php
 
-// thi is the file that handles the uploads into the HM RUSH system
+// this is the file that handles the uploads into the HM RUSH system
 
-/**
- * first we figure out if:
- *      we are going to save a copy locally
- *      whether we need to replicate or not
- *      or do we need to just pass on the file data to a different node altogether
- *
- * 
- * get the cluster and node config
- * parse the url for the input params of:
- *      file name to use when the file is saved
- *      number of times to replicate the file
- *      the params will be located in the path info
- *
- *          /filename/num_times_to_replicate
- *
- *      if num_times_to_replicate is not there, then we assume that we need to replicate
- *      the max_replica_count - 1
- *
- * get all of the urls for the image
- * check to see if this node is included in the urls
- * if not
- *      then we choose the first url in the list
- *      test to see if the url is available via an OPTIONS request
- *      if available send the file data there under the filename we parsed from the url.
- *      if not avaialble, try the next url in the list
- *      repeat until the list is exhausted,  if we did not manage
- *      to send the file anywhere, then we need to return a 500 error
- *
- *      we omit the num_times_to_replicate because we know that
- *      the node to which we are forwarding will take care of starting the replication
- *
- *      to facilitate the forward we read from stdin
- *      and use that with the curl lib to make a put request
- *      to the next node.
- * 
- * if the url is in the list, that means we need to save the file to disk
- *      spool the file to disk to a temp spot
- *      rename the file to the name we parsed from the url
- *      this is where we need to send back a signal to the client
- *      that we are all good and we need to disconnect from the client
- *      but keep running to complete the replication checks below
- *
- *      now we check to see if we should replicate
- *          check the num_times_to_replicate
- *
- *          if num_times_to_replicate is 0
- *              do nothing
- *
- *          if num_times_to_replicate is null or non-existent or the empty string
- *              then we assume we are the first stop and need to
- *              start the full replication
- *
- *          if num_times_to_replicate is greater than 0
- *              we take the total number of urls for replicas
- *              subtract num_times_to_replicate,
- *                  this will yield the index of the url to which we should forward
- *
- *              we get the url from our url list
- *              subtract 1 from num_times_to_replicate
- *              forward on to the url with the total num_times_to_replicate
- *
- */
-require("PHPDFS.php");
-require_once("PHPDFS/DataLocator/HonickyMillerR.php");
+require_once("PHPDFS.php");
 require_once("PHPDFS/Helper.php");
 
 $config = PHPDFS_Helper::getConfig();
-$hm = new PHPDFS_DataLocator_HonickyMillerR( $config );
 $params = PHPDFS_Helper::getParamsFromUrl();
 
-$dfs = new PHPDFS( $hm, $config, $params );
-$dfs->spoolFile();
-PHPDFS_Helper::disconnectClient();
+$dfs = new PHPDFS( $config, $params );
+
+// get the data from stdin and put it in a temp file
+// we will disconnect the client at this point and continue
+// with replication and data forwarding if we are configged to do so
+// otherwise we hang on to the client, which in most cases is really bad
+// because you stay connected until the replication chain is completed
+$dfs->spoolData( );
+
+// save the data to the appropriate directory and emove the spooled file
+// but only if we are a targetNode, otherwise DO NOTHING
 $dfs->saveData( );
+
+// forward the data on to the next node
+// the reasons for forwarding are:
+//
+// we are NOT a target node and are just the first node
+// to receive the upload, so we forward the data to the first targetNode
+// and remove the spooled file
+//
+// or we are a targetNode and need to fulfill the replication requirements
+// and forward data to the next targetNode in our list.
+// if we are the last replication target, we DO NOTHING.
 $dfs->forwardData( );
 
