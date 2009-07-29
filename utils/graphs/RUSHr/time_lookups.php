bdd877ccd32016d1ec57997137a3564b99f6d10e
shanehill00 <shanehill00@10444eee-95d0-11de-99ed-9d3e36ffeeb7>
2009-07-29 23:58:39 +0000

    adding the test scripts, graphs and images for testing the RUSHr implementation

---
+++ utils/graphs/RUSHr/time_lookups.php
@@ -0,0 +1,58 @@
+<?php
+// the number of objects to generate and test for data distribution
+$totalObjs = isset($argv[1]) ? $argv[1] : 1000;
+
+// now we loop on each config
+// and take stats on the distribution
+// and data movement
+$configs = makeConfigs( 99 );
+
+$totalConfs = count($configs);
+$timings = array();
+require_once 'PHPDFS/DataLocator/RUSHr.php';
+
+for($n = 0; $n < $totalConfs; $n++){
+    echo("processing conf $n with ".count( $configs[$n]['clusters'] )." clusters.\n");
+    $totalTime = 0;
+    $hm = new PHPDFS_DataLocator_RUSHr( $configs[$n] );
+    for( $i = 0; $i < $totalObjs; $i++ ){
+        $objKey = uuid_create();
+        $time = microtime(1);
+        $hm->findNode( $objKey );
+        $time2 = microtime(1);
+        $totalTime += $time2 - $time;
+    }
+    $timings[$n] = ( $totalTime / $totalObjs );
+    echo("avgtime:". $timings[$n]."\n");
+}
+print_r( $timings );
+
+function makeConfigs( $numConfigs = 1 ){
+    $configs = array();
+    $replicationDegree = 3;
+    for( $num = 0; $num < $numConfigs; $num++ ){
+        $configs[$num] = array(
+            'replicationDegree' => $replicationDegree,
+            'clusters' => array(),
+        );
+        $weight = 1;
+        $disk = 1;
+        for( $cluster = 0; $cluster < ($num + 1); $cluster++ ){
+            $nodes = array();
+            for( $n = 0; $n < 5; $n++ ){
+                $nodes[] = $disk++;
+            }
+            $configs[ $num ]['clusters'][$cluster] = array(
+                'weight' => $weight,
+                'nodes' => $nodes,
+            );
+            
+            if( $weight > 1 ){
+                $weight = pow( $weight, 1.1 );
+            } else {
+                $weight = 2;
+            }
+        }
+    }
+    return $configs;
+}
