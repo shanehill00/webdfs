53e7cc726d5e98d02505b46df9ddca894733926e
shanehill00 <shanehill00@10444eee-95d0-11de-99ed-9d3e36ffeeb7>
2009-08-16 05:21:09 +0000

    oops issued a bad copy name.  thats what you get for drinking too much imperial stout while coding.  oh well,  there really are no mistakes, just happy accidents

---
+++ tags/phpdfs-0.07/library/PHPDFS/Client.php
@@ -0,0 +1,196 @@
+<?php
+/**
+Copyright (c) 2009, Shane Hill <shanehill00@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain
+      the above copyright notice, this list of conditions
+      and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+class PHPDFS_Client{
+
+    /**
+     * the data locator that is used for looking up the
+     * location of an object.
+     *
+     * @var <PHPDFS_DataLocator_HonickyMillerR>
+     */
+    
+    protected $locator = null;
+    protected $config = null;
+
+    const PHPDFS_PUT_ERR = 1;
+    const PHPDFS_DELETE_ERR = 2;
+    const PHPDFS_GET_ERR = 3;
+
+    protected $errs;
+
+    public function __construct( $config ){
+        $config = $config['data'][0];
+        require_once( $config['locatorClassPath'] );
+        $locatorClassName = $config['locatorClassName'];
+        $this->locator = new $locatorClassName( $config );
+        $this->config = $config;
+
+        $this->errs = array(
+            self::PHPDFS_PUT_ERR => array(
+                'className' => 'PHPDFS_Exception_PutException',
+                'msg'   => 'PUT Exception',
+                'classPath' => 'PHPDFS/Exception/PutException.php'
+            ),
+            self::PHPDFS_DELETE_ERR => array(
+                'className' => 'PHPDFS_Exception_DeleteException',
+                'msg'   => 'DELETE Exception',
+                'classPath' => 'PHPDFS/Exception/DeleteException.php'
+            ),
+            self::PHPDFS_GET_ERR => array(
+                'className' => 'PHPDFS_Exception_GetException',
+                'msg'   => 'GET Exception',
+                'classPath' => 'PHPDFS/Exception/GetException.php'
+            ),
+        );
+    }
+
+    /**
+     * fetches the entity and returns a ref to it
+     *
+     *
+     * @param <string> $fileId
+     *
+     * @throws PHPDFS_Client_GetException
+     */
+    public function &get( $fileId ){
+        $paths = $this->getPaths($fileId);
+        $response = "";
+        if( count( $paths ) ) {
+            $url = $paths[0]['url'];
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+            curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_BINARYTRANSFER, true);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+            $response = curl_exec($curl);
+            $this->checkError( $curl, self::PHPDFS_GET_ERR, $response );
+        }
+        return $response;
+    }
+
+    public function getPaths( $fileId ){
+        $paths = array();
+        $nodes = $this->locator->findNodes( $fileId );
+        foreach( $nodes as $node ){
+            $data = array();
+            $data['url'] = $node['proxyUrl'].'/'.$fileId;
+            $data['proxyUrl'] = $node['proxyUrl'];
+            $paths[] = $data;
+        }
+        return $paths;
+    }
+
+    /**
+     *
+     * @param <string> $fileId
+     * @throws PHPDFS_Client_GetException
+     */
+    public function delete( $fileId ){
+        $paths = $this->getPaths($fileId);
+        $url = $paths[0]['url'];
+        if( count( $paths ) ) {
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($curl);
+            $this->checkError( $curl, self::PHPDFS_DELETE_ERR, $response );
+        }
+    }
+
+    /**
+     * @param <string> $fileId
+     * @param <string> $filePath
+     *
+     * @throws PHPDFS_Client_PutException
+     */
+    public function put( $fileId, $filePath ){
+        $this->set( $fileId, $filePath );
+    }
+
+    /**
+     * @param <string> $fileId
+     * @param <string> $filePath
+     *
+     * @throws PHPDFS_Client_PutException
+     */
+    public function set( $fileId, $filePath ){
+        $paths = $this->getPaths($fileId);
+        if( count( $paths ) ) {
+            $url = $paths[0]['url'];
+            $fh = fopen($filePath, "rb");
+            $size = filesize( $filePath );
+            rewind($fh);
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_INFILE, $fh);
+            curl_setopt($curl, CURLOPT_INFILESIZE, $size );
+            curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+            curl_setopt($curl, CURLOPT_PUT, 4);
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($curl);
+            $this->checkError( $curl, self::PHPDFS_PUT_ERR, $response );
+            fclose($fh);
+        }
+    }
+
+    /**
+     * @param <curl> $curl
+     * @param <int> $phpDfsErrCode
+     * @param <reference> $additionalInfo
+     */
+    protected function checkError( $curl, $phpDfsErrCode, &$additionalInfo = "" ){
+        $info = curl_getinfo( $curl );
+        $isHttpErr =  isset( $info['http_code'] ) && ( $info['http_code'] >= 400 );
+        $isOtherErr = curl_errno($curl);
+        $data = array();
+        if( $isOtherErr || $isHttpErr ){
+            $errInfo = $this->errs[ $phpDfsErrCode ];
+            require_once( $errInfo['classPath'] );
+            $exceptionClass = $errInfo['className'];
+            $data['httpCode'] = isset($info['http_code']) ? $info['http_code'] : '';
+            $data['url'] = isset($info['url']) ? $info['url'] : '';
+            $data['contentType'] = isset($info['content_type']) ? $info['content_type'] : '';
+            $data['body'] = $additionalInfo;
+            //[url] => http://www.google.com/dfgsdfgsfg
+            //[content_type] => text/html; charset=ISO-8859-1
+            //[http_code] => 404
+            if( $isOtherErr ){
+                $msg = $phpDfsErrCode." - ".$errInfo['msg']. " : ".curl_errno($curl)." - " .curl_error($curl)." : $additionalInfo";
+            } else {
+                $msg = "http error! ".$info['http_code'];
+            }
+            throw new $exceptionClass( $msg, $data );
+        }
+    }
+}
---
+++ tags/phpdfs-0.07/utils/java/PHPDFSTestClient/src/phpdfstestclient/PHPDFSTestClient.java
@@ -0,0 +1,424 @@
+package phpdfstestclient;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.FileRequestEntity;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PutMethod;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+
+/**
+ * This is a simple http client that will be used for testing phpdfs.
+ *
+ * the algorithm is as follows:
+ *
+ * start the client
+ * the client will alternate between uploads and downloads
+ *  randomly select from the list of possible files to upload
+ *  generate a unique name using the UUID class
+ *  upload the file
+ *  put the name of the file in an index (hashmap)
+ *  upon successful upload
+ *      randomly choose a file from those that we have uploaded
+ *      download the file,
+ *      note the statistics of the download (we will not be saving the file)
+ *          size, name, time to download, successful or not
+ * 
+ *
+ *
+ * @author shane
+ */
+public class PHPDFSTestClient extends Thread {
+
+    private long time = 0;
+    private long bytesToUpload = 0;
+    private int getsPerUpload = 0;
+    static AtomicInteger numThreads = new AtomicInteger(0);
+
+    private long myTotalBytesUp = 0;
+    private long myTotalBytesDown = 0;
+    private long myTotalBytes = 0;
+    
+    private long myTotalRequestsUp = 0;
+    private long myTotalRequestsDown = 0;
+    private long myTotalRequests = 0;
+
+    private long myTotalFailuresUp = 0;
+    private long myTotalFailuresDown = 0;
+    private long myTotalFailures = 0;
+
+    private long myTotalTimeUp = 0;
+    private long myTotalTimeDown = 0;
+    private long myTotalTime = 0;
+
+    private static AtomicLong totalBytesUp = new AtomicLong(0);
+    private static AtomicLong totalBytesDown = new AtomicLong(0);
+    private static AtomicLong totalBytes = new AtomicLong(0);
+
+    private static AtomicLong totalRequestsUp = new AtomicLong(0);
+    private static AtomicLong totalRequestsDown = new AtomicLong(0);
+    private static AtomicLong totalRequests = new AtomicLong(0);
+
+    private static AtomicLong totalFailuresUp = new AtomicLong(0);
+    private static AtomicLong totalFailuresDown = new AtomicLong(0);
+    private static AtomicLong totalFailures = new AtomicLong(0);
+
+    private static AtomicLong totalTimeUp = new AtomicLong(0);
+    private static AtomicLong totalTimeDown = new AtomicLong(0);
+    private static AtomicLong totalTime = new AtomicLong(0);
+
+    private static AtomicLong startTime = new AtomicLong(0);
+    private static AtomicLong wallClock = new AtomicLong(0);
+
+    private Random ran = new Random();
+    private HttpClient client = new HttpClient();
+    private ArrayList<String> uploadedFiles = new ArrayList<String>();
+    private ArrayList<String> downloadedFiles = new ArrayList<String>();
+    private ArrayList<String> filesToUpload = null;
+    private ArrayList<String> baseUrls = null;
+
+    public PHPDFSTestClient( long bytesToUpload, int numThreads, int getsPerUpload, String filesToUploadFile, String baseUrlFile ){
+        this.bytesToUpload = bytesToUpload;
+        this.getsPerUpload = getsPerUpload;
+        PHPDFSTestClient.numThreads.compareAndSet(0, numThreads);
+        filesToUpload = fileToArrayList(filesToUploadFile);
+        baseUrls = fileToArrayList(baseUrlFile);
+    }
+
+    /**
+     * opens the passed file and puts each line into an array
+     * @param filename
+     * @return
+     */
+    private static ArrayList<String> fileToArrayList( String filename ){
+        ArrayList<String> lines = new ArrayList<String>();
+        try{
+            BufferedReader br = new BufferedReader( 
+                    new InputStreamReader(
+                        new FileInputStream( filename )
+                    )
+            );
+            
+            String line = null;
+            while( (line = br.readLine() ) != null ){
+                line = line.trim();
+                if( !line.startsWith("//") && line.length() > 0 ){
+                    lines.add(line);
+                }
+            }
+            
+        } catch( FileNotFoundException e ){
+            System.err.println("could not open file " + filename + " for reading! " );
+             e.printStackTrace();
+            System.exit(99);
+        } catch( IOException e ){
+            System.err.println("IOException when processing " + filename );
+             e.printStackTrace();
+            System.exit(99);
+        }
+        return lines;
+    }
+    
+    /**
+     * the client will alternate between uploads and downloads
+     *  randomly select from the list of possible files to upload
+     *  generate a unique name using the UUID class
+     *  upload the file
+     *  put the name of the file in an index (hashmap)
+     *  upon successful upload
+     *      randomly choose a file from those that we have uploaded from the main central hashmap
+     *      download the file,
+     *      note the statistics of the download (we will not be saving the file)
+     *          size, name, time to download, successful or not in the main central hashmap
+     *
+     * there will also be a hashmap that holds all erros that are encounteerd during the test
+     * in the hash map we will put the name of the file that failed, which client and server experienced the failure
+     * and why it failed
+     *
+     * @param url
+     * @param filename
+     * @param type
+     *
+     */
+
+    private void doPut(String url, String filename, String type ){
+
+        // set the method for the client
+        PutMethod method = new PutMethod(url);
+        File file = new File(filename);
+        FileRequestEntity fre = new FileRequestEntity(file, type);
+        method.setRequestEntity(fre);
+
+        // Provide custom retry handler is necessary
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+            new DefaultHttpMethodRetryHandler(1, false));
+
+        try {
+            // Execute the method.
+            time = System.currentTimeMillis();
+            int statusCode = client.executeMethod(method);
+            time = System.currentTimeMillis() - time;
+            myTotalTimeUp += time;
+            myTotalTime += time;
+
+            if (statusCode != HttpStatus.SC_OK && statusCode != HttpStatus.SC_NO_CONTENT ) {
+                myTotalFailuresUp++;
+                myTotalFailures++;
+                System.err.println("Method failed while PUTting " + url + " for thread " + getName() + ": " + method.getStatusLine() );
+            } else {
+                uploadedFiles.add(url);
+
+                myTotalBytesUp += file.length();
+                myTotalRequestsUp++;
+
+                myTotalBytes += file.length();
+                myTotalRequests++;
+            }
+
+        } catch (HttpException e) {
+            System.err.println("Fatal protocol violation while PUTting " + url + " for thread " + getName() + ": " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Fatal transport error while PUTting " + url + " for thread  " + getName() + ": " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Release the connection.
+            method.releaseConnection();
+        }
+    }
+    
+    private void doGet(String url){
+        // set the method for the client
+        GetMethod method = new GetMethod(url);
+
+        // Provide custom retry handler is necessary
+        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+            new DefaultHttpMethodRetryHandler(1, false));
+
+        try {
+            // Execute the method.
+            time = System.currentTimeMillis();
+            int statusCode = client.executeMethod(method);
+            time = System.currentTimeMillis() - time;
+            myTotalTimeDown += time;
+            myTotalTime += time;
+
+            if (statusCode != HttpStatus.SC_OK) {
+                myTotalFailuresDown++;
+                System.err.println("Method failed while GETting " + url + " for thread " + getName() + ": " + method.getStatusLine() );
+            } else {
+                // Read the response body and discard it
+                InputStream responseStream = method.getResponseBodyAsStream();
+                while( responseStream.read( ) != -1 ){ responseStream.skip( 30720000 ); }
+                downloadedFiles.add(url);
+
+                myTotalBytesDown += method.getResponseContentLength();
+                myTotalRequestsDown++;
+
+                myTotalBytes += method.getResponseContentLength();
+                myTotalRequests++;
+            }
+
+        } catch (HttpException e) {
+            System.err.println("Fatal protocol violation while GETting " + url + " for thread " + getName() + ": " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Fatal transport error while GETting " + url + " for thread  " + getName() + ": " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Release the connection.
+            method.releaseConnection();
+        }
+    }
+
+    public void doTest( ){
+        runTest();
+        writeStats();
+    }
+
+    /**
+     * we do the upload and download iterations until we have input
+     * more than the number of bytes passed
+     */
+    private void runTest(){
+        startTime.compareAndSet(0, System.currentTimeMillis() );
+        while( myTotalBytesUp < bytesToUpload ){
+            // randomly select a baseurl and filename to use for an upload
+            String whichUrl = baseUrls.get( ran.nextInt( baseUrls.size() ) ) + "/" + UUID.randomUUID().toString();
+
+            // randomly choose a file for upload
+            String whichFile = filesToUpload.get( ran.nextInt( filesToUpload.size() ) );
+            String fileType = "text/plain";
+
+            doPut( whichUrl, whichFile, fileType );
+
+            for( int i = 0; i < getsPerUpload; i++ ){
+                whichUrl = uploadedFiles.get( ran.nextInt( uploadedFiles.size() ) );
+                doGet(whichUrl);
+            }
+        }
+    }
+
+    private void writeStats(){
+        totalBytesUp.addAndGet(myTotalBytesUp);
+        totalBytesDown.addAndGet(myTotalBytesDown);
+        totalBytes.addAndGet(myTotalBytes);
+
+        totalRequestsUp.addAndGet(myTotalRequestsUp);
+        totalRequestsDown.addAndGet(myTotalRequestsDown);
+        totalRequests.addAndGet(myTotalRequests);
+
+        totalFailuresUp.addAndGet(myTotalFailuresUp);
+        totalFailuresDown.addAndGet(myTotalFailuresDown);
+        totalFailures.addAndGet(myTotalFailures);
+
+        totalTimeUp.addAndGet(myTotalTimeUp);
+        totalTimeDown.addAndGet(myTotalTimeDown);
+        totalTime.addAndGet(myTotalTime);
+        long end = System.currentTimeMillis();
+        //endTime.getAndSet( end );
+        wallClock.set(end - startTime.get());
+
+        /**
+        System.out.println( "Report for thread " + getName() );
+        System.out.println( "Requests: " );
+        System.out.println( "  up: " + myTotalRequestsUp );
+        System.out.println( "  down: " + myTotalRequestsDown );
+        System.out.println( "  total: " + myTotalRequests );
+        System.out.println( "" );
+
+        System.out.println( "Bytes: " );
+        System.out.println( "  up: " + myTotalBytesUp );
+        System.out.println( "  down: " + myTotalBytesDown );
+        System.out.println( "  total: " + myTotalBytes );
+        System.out.println( "" );
+
+        System.out.println( "Failures: " );
+        System.out.println( "  up: " + myTotalFailuresUp );
+        System.out.println( "  down: " + myTotalFailuresDown );
+        System.out.println( "  total: " + myTotalFailures );
+        System.out.println( "" );
+
+        System.out.println( "Time: " );
+        System.out.println( "  up: " + myTotalTimeUp );
+        System.out.println( "  down: " + myTotalTimeDown );
+        System.out.println( "  total: " + myTotalTime );
+        System.out.println( "" );
+
+        System.out.println( "Requests / Sec: " );
+        System.out.println( "  up: " + ( (float)myTotalRequestsUp / ((float)myTotalTimeUp / (float)1000 )) );
+        System.out.println( "  down: " + ( (float)myTotalRequestsDown / ((float)myTotalTimeDown / (float)1000 )) );
+        System.out.println( "  avg: " + ( (float)myTotalRequests / ((float)myTotalTime / (float)1000 )) );
+        System.out.println( "" );
+
+        System.out.println( "Throughput - Bytes / Sec: " );
+        System.out.println( "  up: " + ( (float)myTotalBytesUp / ((float)myTotalTimeUp / (float)1000 )) );
+        System.out.println( "  down: " + ( (float)myTotalBytesDown / ((float)myTotalTimeDown / (float)1000 )) );
+        System.out.println( "  total: " + ( (float)myTotalBytes / ((float)myTotalTime / (float)1000 )) );
+        System.out.println( "" );
+        System.out.println( "_____________________________" );
+        System.out.println( "" );
+         */
+        System.out.println("completed " + totalRequests.get() );
+        
+        if( numThreads.decrementAndGet() == 0 ){
+
+            String report =
+             "========= Overall Report =========\n" +
+             "Requests: \n" +
+             "  up: " + totalRequestsUp.get() + "\n" +
+             "  down: " + totalRequestsDown.get() + "\n" +
+             "  total: " + totalRequests.get() + "\n" +
+             "\n" +
+
+             "Bytes: \n" +
+             "  up: " + totalBytesUp.get() + "\n" +
+             "  down: " + totalBytesDown.get() + "\n" +
+             "  total: " + totalBytes.get() + "\n" +
+             "\n" +
+
+             "Failures: \n" +
+             "  up: " + totalFailuresUp.get() + "\n" +
+             "  down: " + totalFailuresDown.get() + "\n" +
+             "  total: " + totalFailures.get() + "\n" +
+             "\n" +
+
+             "Time: \n" +
+             "  up: " + totalTimeUp.get() + "\n" +
+             "  down: " + totalTimeDown.get() + "\n" +
+             "  total: " + totalTime.get() + "\n" +
+             "  wall clock: " + wallClock.get() + "\n" +
+             "\n" +
+
+             "Requests / Sec: \n" +
+             "  mean up: " + ( (float) totalRequestsUp.get() / ((float)totalTimeUp.get() / (float)1000 ) ) + "\n" +
+             "  mean down: " + ( (float)totalRequestsDown.get() / ( (float)totalTimeDown.get() / (float)1000 ) ) + "\n" +
+             "  mean total: " + ( (float)totalRequests.get() / ((float)totalTime.get() / (float)1000 )) + "\n" +
+             "  mean total (across all requests): " + ( (float)totalRequests.get() / ((float)wallClock.get() / (float)1000 ) ) + "\n" +
+             "\n" +
+
+             "Throughput - Bytes / Sec: \n" +
+             "  mean up: " + ( (float) totalBytesUp.get() / ((float)totalTimeUp.get() / (float)1000 ) ) + "\n" +
+             "  mean down: " + ( (float)totalBytesDown.get() / ( (float)totalTimeDown.get() / (float)1000 ) ) + "\n" +
+             "  mean total: " + ( (float)totalBytes.get() / ( (float)totalTime.get() / (float)1000 ) ) + "\n" +
+             "  mean total (across all requests): " + ( (float)totalBytes.get() / ( (float)wallClock.get() / (float)1000 ) ) + "\n" +
+             "\n" +
+             "_____________________________\n" +
+             "\n";
+            System.out.println( report );
+            try{
+                File statsFile = new File("/tmp/phpdfs.stats.txt");
+                FileWriter stats = new FileWriter(statsFile);
+                stats.write(report);
+                stats.flush();
+            } catch( IOException e ) {
+                e.printStackTrace();
+                System.exit(99);
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        doTest();
+    }
+
+    public static void main( String args[] ){
+        int getsPerUpload = 1;
+        int threads = 50;
+        long bytesToUploadPerThread = 2048000L;
+        String filesToUpload = "/Users/shane/dev/phpdfs/utils/filesToUpload";
+        String baseUrls = "/Users/shane/dev/phpdfs/utils/baseUrls";
+
+        if( args.length == 5 ){
+            threads = Integer.parseInt( args[0] );
+            bytesToUploadPerThread = Long.parseLong( args[1] );
+            getsPerUpload = Integer.parseInt( args[2] );
+            filesToUpload = args[3];
+            baseUrls = args[4];
+        }
+        
+        for( int n = 0; n < threads; n++ ){
+            PHPDFSTestClient thw = new PHPDFSTestClient( bytesToUploadPerThread, threads, getsPerUpload, filesToUpload, baseUrls );
+            thw.start();
+        }
+    }
+}
+
