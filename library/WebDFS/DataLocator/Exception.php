480dca49c12a3d785649f5e599e680148f86eb10
shanehill00 <shanehill00@10444eee-95d0-11de-99ed-9d3e36ffeeb7>
2009-09-04 18:49:07 +0000

    fix an include statement

--- library/WebDFS/DataLocator/Exception.php
+++ library/WebDFS/DataLocator/Exception.php
@@ -31,4 +31,4 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * @author Shane Hill <shanehill00@gmail.com>
  */
 
-class PHPDFS_DataLocator_Exception extends Exception {   }
+class WebDFS_DataLocator_Exception extends Exception {   }
